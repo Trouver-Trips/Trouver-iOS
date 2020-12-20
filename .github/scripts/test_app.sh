bb93d71f89ea27c799d78496b47afad04cbbf59d
Sagar Punhani <sagar7p@gmail.com>
2020-12-20 15:23:55 -0600

    Fixing simulator name

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace src/Trouver/Trouver.xcworkspace \
             -scheme Trouver \
-            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 12 \
+            -destination platform=iOS\ Simulator,OS=14.2,name=iPhone\ 12 \
             clean test | xcpretty
