6129839c4593290b51fe6e97145e2653925a818f
Sagar Punhani <sagar7p@gmail.com>
2020-12-20 15:22:15 -0600

    Fixing scheme name

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace src/Trouver/Trouver.xcworkspace \
-            -scheme Trouver\ iOS \
+            -scheme Trouver \
             -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 12 \
             clean test | xcpretty
