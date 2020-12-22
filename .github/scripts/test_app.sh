a1862b7ec9d65c96ffefcd4fce95d7f18455d413
Sagar Punhani <sagar7p@gmail.com>
2020-12-22 13:04:00 -0600

    Fixing new lines

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -6,4 +6,3 @@ xcodebuild -workspace src/Trouver/Trouver.xcworkspace \
             -scheme Trouver \
             -destination platform=iOS\ Simulator,OS=14.2,name=iPhone\ 12 \
             clean test | xcpretty
-            
