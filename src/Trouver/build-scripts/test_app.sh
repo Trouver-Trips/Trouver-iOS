a92ae79e10d8ae78abec6fd5624a03aa33e6dbc7
Sagar Punhani <sagar7p@gmail.com>
2021-01-04 19:28:51 -0600

    Finished renaming

--- src/Trouver/build-scripts/test_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace ./../Trouver.xcworkspace \
-            -scheme Trouver \
-            -destination platform=iOS\ Simulator,OS=14.3,name=iPhone\ 12 \
-            clean test | xcpretty
