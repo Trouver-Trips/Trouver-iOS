33093a1d8f5da480cac41d7dbbf6a9df568ae2ef
Sagar Punhani <sagar7p@gmail.com>
2021-09-06 16:29:08 -0700

    Update detail page

--- src/Trouver/Detail/Models/HikeDetail.swift
+++ src/Trouver/Detail/Models/HikeDetail.swift
@@ -10,6 +10,16 @@ import CoreGraphics
 
 // Model for Hiking Details
 struct HikeDetail {
+    private let prepositions: Set<String> = [
+        "in",
+        "via",
+        "to",
+        "from",
+        "at",
+        "via",
+        "&"
+    ]
+    
     let id: String
     let name: String
     let imageUrls: [URL]
@@ -18,24 +28,40 @@ struct HikeDetail {
     let rating: CGFloat
     let location: Location
     let difficulty: Difficulty
-    let elevationGain: Double
+    let elevationGain: Int
     let length: Double
     let popularity: Double
     let routeType: RouteType
+    
+    var shortName: String {
+        var comps = self.name.components(separatedBy: .whitespacesAndNewlines)
+            .filter { !$0.isEmpty }[..<3]
+            
+        if let deleteIndex = comps.firstIndex(where: { prepositions.contains($0) }) {
+            comps = comps[0..<deleteIndex]
+        }
+        
+        if let deleteIndex = comps.firstIndex(where: { $0.contains(",") }) {
+            comps[deleteIndex].removeAll { $0 == "," }
+            comps = comps[0...deleteIndex]
+        }
+        
+        return comps.joined(separator: " ")
+    }
 }
 
 extension HikeDetail {
     init(dto: HikeDetailDTO) {
         self.id = dto.id ?? UUID().uuidString
-        self.name = dto.name ?? "No name found"
+        self.name = dto.name ?? Localization.string(for: "hike.unknown.title")
         self.imageUrls = dto.images?.compactMap { URL(string: $0) } ?? []
         self.attributes = dto.attributes ?? []
-        self.description = dto.hikeDescription ?? "No description found"
+        self.description = dto.hikeDescription ?? Localization.string(for: "description.unknown.title")
         self.rating = CGFloat(dto.rating ?? 0)
         self.location = dto.location.map { Location(dto: $0) } ?? .init(coordinates: [])
         self.difficulty = Difficulty(from: dto.difficulty ?? 0)
-        self.elevationGain = dto.elevationGain ?? 0
-        self.length = dto.length ?? 0
+        self.elevationGain = Int((dto.elevationGain ?? 0).toFeet)
+        self.length = (dto.length ?? 0).toMiles
         self.popularity = dto.popularity ?? 0
         self.routeType = RouteType(rawValue: dto.routeType ?? "") ?? .unknown
     }
--- src/Trouver/Detail/Views/DetailView.swift
+++ src/Trouver/Detail/Views/DetailView.swift
@@ -9,17 +9,14 @@ import SwiftUI
 
 struct DetailView: View {
     private enum Constants {
-        static let imageSize: CGFloat = 24
+        static let titleSize: CGFloat = 60
+        static let shadowRadius: CGFloat = 8
         static let sidePadding: CGFloat = 32
-        static let topPadding: CGFloat = 48
-        static let bottomPadding: CGFloat = 24
+        static let screenWidth: CGFloat = UIScreen.main.bounds.size.width
+        static let sectionPadding: CGFloat = 12
+        static let bottomPadding: CGFloat = 12
     }
     
-    private var screenWidth: CGFloat {
-        UIScreen.main.bounds.size.width
-    }
-    
-    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @EnvironmentObject var loginViewModel: LoginService
     @Binding var isPresented: Bool
     @ObservedObject var viewModel: HikeDetailProvider
@@ -27,25 +24,59 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            VStack {
+            Group {
                 switch viewModel.state {
                 case .idle: EmptyView()
-                case .loading:
-                    HStack {
-                        Spacer()
-                        ProgressView()
-                            .padding(.top, Constants.topPadding)
-                        Spacer()
-                    }
+                case .loading: ProgressView()
                 case .loaded(let hikeDetail):
-                    ImagePageView(width: screenWidth,
-                                  images: hikeDetail.imageUrls,
-                                  startingImageIndex: startingImageIndex)
-                    Text(hikeDetail.name)
-                        .padding()
-                    Text("description.title")
-                        .padding()
-                    Text(hikeDetail.description)
+                    VStack(spacing: Constants.sectionPadding) {
+                        ZStack(alignment: .leading) {
+                            ImagePageView(width: Constants.screenWidth,
+                                          images: hikeDetail.imageUrls,
+                                          startingImageIndex: startingImageIndex)
+                            VStack(alignment: .leading) {
+                                Spacer()
+                                Text(hikeDetail.shortName)
+                                    .fontWeight(.bold)
+                                    .shadow(radius: Constants.shadowRadius)
+                                    .font(.system(size: Constants.titleSize))
+                                    .minimumScaleFactor(0.01)
+                                    .lineLimit(1)
+                                    .rotationEffect(.degrees(-90), anchor: .topLeading)
+                                    .padding(.bottom, Constants.bottomPadding)
+                            }
+                            .padding(.leading, Constants.sidePadding)
+                        }
+                        Group {
+                            HStack {
+                                IconLabel(image: "triangle", key: "units.feet.measurement \(hikeDetail.elevationGain)")
+                                Spacer()
+                                IconLabel(image: "map",
+                                          key: "units.miles.measurement \(hikeDetail.length, specifier: "%.1f")")
+                                Spacer()
+                                switch hikeDetail.difficulty {
+                                case .unknown:
+                                    CapsuleView(text: "difficulty.option.unknown", color: .accentColor)
+                                case .easy:
+                                    CapsuleView(text: "difficulty.option.easy", color: .green)
+                                case .moderate:
+                                    CapsuleView(text: "difficulty.option.moderate", color: .yellow)
+                                case .hard:
+                                    CapsuleView(text: "difficulty.option.hard", color: .red)
+                                }
+                            }
+                            VStack(alignment: .leading) {
+                                Text("description.title")
+                                    .font(.title)
+                                    .padding(.vertical)
+                                HStack {
+                                    Text(hikeDetail.description)
+                                    Spacer()
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
                 case .error:
                     HStack {
                         Text("generic.error")
@@ -53,43 +84,8 @@ struct DetailView: View {
                 }
             }
             .overlay(
-                VStack {
-                    HStack {
-                        Button(action: {
-                            isPresented = false
-                            presentationMode.wrappedValue.dismiss()
-                        }, label: {
-                            Image(systemName: "chevron.backward")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: Constants.imageSize,
-                                       height: Constants.imageSize)
-                                .foregroundColor(Color.foregroundColor)
-                        })
-                        Spacer()
-                        if case .loaded(_) = viewModel.state {
-                            Button(action: {
-                                viewModel.toggleFavorite()
-                            }, label: {
-                                Image(systemName: viewModel.isFavorite ? "suit.heart.fill" : "suit.heart")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: Constants.imageSize,
-                                           height: Constants.imageSize)
-                                    .foregroundColor(Color.foregroundColor)
-                            })
-                        }
-                    }
-                    .padding(.horizontal, Constants.sidePadding)
-                    .padding(.top, Constants.topPadding)
-                    .padding(.bottom, Constants.bottomPadding)
-                    .background(
-                        LinearGradient(gradient:
-                                        Gradient(colors: [Color.black.opacity(0.8),
-                                                          Color.black.opacity(0)]),
-                                       startPoint: .top, endPoint: .bottom)
-                    )
-                    Spacer()
+                DetailNavBar(isPresented: $isPresented, isFavorite: viewModel.isFavorite) {
+                    viewModel.toggleFavorite()
                 }
             )
             .background(Color.themeColor)
